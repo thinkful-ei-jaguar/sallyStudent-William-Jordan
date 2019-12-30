d7693716f19faa75fcc216a8b04673feccc1a8ce
Jordan Castillo <jordanxcallaway@gmail.com>
2019-12-30 13:52:14 -0800

    starting styling

--- sal/src/ContactInfo.js
+++ sal/src/ContactInfo.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import css from './index.css';
 
 function ContactInfo(props) {
   return (
     <div className="Contact-Info">
       <ul className="Info-List">
-        <li id="name">Name: {props.name}</li>
-        <li id="email">E-mail: {props.email}</li>
-        <li id="phone">Phone #: {props.phone}</li>
         <li id="picture">
           <img src={props.picture} />
         </li>
+        <div className="contact">
+          {/* <li id="name">Name: {props.name}</li> */}
+          <li id="email">E-mail: {props.email}</li>
+          <li id="phone">Phone #: {props.phone}</li>
+        </div>
       </ul>
     </div>
   );
