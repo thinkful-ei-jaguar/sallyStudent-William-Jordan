c2a83c761e9edf9628347c9241d58c5565094bb5
Bilbertius <lauchlinmacneil@gmail.com>
2019-12-30 10:29:42 -0700

    Add AppHeader and ContactInfo

---
+++ sally/src/AppHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AppHeader() {
+  return (
+    <header className="app-header">
+      <h1>Sally Student</h1>
+    </header>
+  );
+}
+
+export default AppHeader;
